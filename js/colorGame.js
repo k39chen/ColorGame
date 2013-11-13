bceb56a2042ad9c3ce4c81b60a93347d8a2026c7
Kevin Chen <kchen@tagged.com>
2013-11-12 23:27:09 -0800

    Preliminary work on game logic

--- js/colorGame.js
+++ js/colorGame.js
@@ -2,15 +2,18 @@ $(document).ready(function(){
 
 	var canvas = $("#canvas");
 
+	// initialize UI elements
 	$(".button").css({userSelect:"none"}).easyHover({
 		start: {color:"rgb(200,200,200)",lineHeight:"1.0em",fontSize:18},
 		end: {color:"rgb(255,255,255)",lineHeight:"0.9em",fontSize:22}
 	});
 
+	// create and start the game object
 	var game = new Game(canvas);
 
 	game.start();
 
+	// initialize the new game button functionality
 	$("#newgame-btn").click(function(){
 		if (!game.locked) {
 			game.restart();
@@ -19,22 +22,26 @@ $(document).ready(function(){
 
 });
 
-var OPACITY_NORMAL = 0.4;
+// game constants
+var OPACITY_NORMAL = 0.6;
 var OPACITY_CONTROLLED = 0.0;
 var OPACITY_HOVER = 0.0;
 
 function Game(canvas) {
 
+	// reference to own object
 	var self = this;
 
+	// flag for if we can interact with the UI
 	this.locked = true;
 
-	// constructor
-	this.anchor = null;
+	// game components
 	this.canvas = canvas;
 	this.startMarker = $("#start-marker");
 	this.turnsCounter = $("#turns-container");
 	this.completionCounter = $("#completion-container");
+
+	// board constants
 	this.board = {
 		width: 16,
 		height: 8,
@@ -43,52 +50,85 @@ function Game(canvas) {
 		animDuration: 250,
 		seed: 60
 	};
+
+	// tile constants
 	this.tile = {
 		borderRadius: "5px",
 		width: 32,
 		height: 32,
-		scaleBy: 1.3,
+		scaleBy: 1.2,
 		animDuration: 200
 	};
+
+	// set of colors
 	this.numColors = 5;
 	this.colors = [];
+	this.currentColor = -1;
 
+	// game metrics
 	this.numTiles = 0;
 	this.numTurns = 0;
 	this.totalTiles = 0;
 	this.totalTurns = 200;
 
+	// cached game metrics
 	this._numTiles = -1;
 	this._numTurns = -1;
 	this._totalTiles = -1;
 	this._totalTurns = -1;
 
+	// game variables
+	this.anchor = null;
+	this.controlled = [];
+	// this.adjacentTiles = [];
+
 	this.start = function() {
+
+		// specify animation parameters
+		var o = {opacity:0.0}, 
+			o_ = {opacity:1.0}, 
+			d = self.board.animDuration;
+
 		// first we must create the board!
 		self.clear();
 		_createBoard();
-		self.startMarker.css({opacity:0.0}).stop().animate({opacity:1.0},self.board.animDuration);
-		self.turnsCounter.css({opacity:0.0}).stop().animate({opacity:1.0},self.board.animDuration);
-		self.completionCounter.css({opacity:0.0}).stop().animate({opacity:1.0},self.board.animDuration);
-		self.canvas.css({opacity:0.0}).stop().animate({opacity:1.0},self.board.animDuration,function(){
+
+		// reset the number of turns taken
+		this.numTurns = 0;
+
+		// fade everything into existence
+		self.startMarker.css(o).stop().animate(o_,d);
+		self.turnsCounter.css(o).stop().animate(o_,d);
+		self.completionCounter.css(o).stop().animate(o_,d);
+		self.canvas.css(o).stop().animate(o_,d,function(){
 			self.locked = false;
 		});
 
+		// update the HUD, don't animate the text!
 		_updateHud(true);
 	};
 
 	this.restart = function() {
+		// clear the board and recreate it again
 		self.clear(function(){
 			self.start()
 		});
 	};
 
 	this.clear = function(cb){
+		// define animation parameters
+		var o = {opacity:1.0}, 
+			o_ = {opacity:0.0}, 
+			d = self.board.animDuration;
+
+		// lock UI
 		self.locked = true;
-		self.startMarker.css({opacity:1.0}).stop().animate({opacity:0.0},self.board.animDuration);
-		self.turnsCounter.css({opacity:1.0}).stop().animate({opacity:0.0},self.board.animDuration);
-		self.completionCounter.css({opacity:1.0}).stop().animate({opacity:0.0},self.board.animDuration);
-		self.canvas.css({opacity:1.0}).stop().animate({opacity:0.0},self.board.animDuration,function(){
+
+		// animate the UI out of existence
+		self.startMarker.css(o).stop().animate(o_,d);
+		self.turnsCounter.css(o).stop().animate(o_,d);
+		self.completionCounter.css(o).stop().animate(o_,d);
+		self.canvas.css(o).stop().animate(o_,d,function(){
 			self.canvas.empty();
 			if (cb) cb();
 		})
@@ -118,13 +158,14 @@ function Game(canvas) {
 					if (Math.floor(Math.random()*100) > self.board.seed) continue; 
 				}
 
+				// specify tile properties
 				var pos = {x:x,y:y};
 				var coord = _getCoord(x,y);
 				var colorIndex = Math.floor(Math.random()*self.numColors);
 				var posStyles = {
 					position: "absolute",
 					borderRadius: self.tile.borderRadius,
-					borderTop: "solid 1px rgb(180,180,180)",
+					borderTop: "solid 1px rgb(100,100,100)",
 					borderBottom: "solid 1px rgb(50,50,50)",
 					width: self.tile.width,
 					height: self.tile.height,
@@ -132,20 +173,25 @@ function Game(canvas) {
 					top: coord.y,
 				};
 
+				// create the tile object
 				var tile = $("<div>")
 					.addClass("tile")
 					.attr("pos","("+[x,y]+")")
 					.data("pos",pos)
 					.data("colorIndex",colorIndex)
-					.css($.extend({ background: "rgb("+self.colors[colorIndex]+")"}, posStyles))
+					.css($.extend({ backgroundColor: "rgb("+self.colors[colorIndex]+")"}, posStyles))
 					.appendTo(self.canvas);
+
+				// create the corresponding tile mask
 				var mask = $("<div>")
 					.addClass("tile-mask")
 					.attr("pos","("+[x,y]+")")
 					.data("pos",pos)
-					.css($.extend({opacity:OPACITY_NORMAL, background:"black",cursor:"pointer"}, posStyles))
+					.data("colorIndex",colorIndex)
+					.css($.extend({opacity:OPACITY_NORMAL, backgroundColor:"black",cursor:"pointer"}, posStyles))
 					.appendTo(self.canvas);
 
+				// define mouse over behaviour
 				mask.mouseover(function(){
 					var a = _getAnimProperties($(this)),
 						p = {width:a.w,height:a.h,left:a.c.x,top:a.c.y},
@@ -156,6 +202,8 @@ function Game(canvas) {
 					$(this).stop().css($.extend(o,p)).animate($.extend(o_,p_), a.d);
 					_getTile(a.p).stop().css(p).animate(p_, a.d);
 				});
+
+				// define mouse out behaviour
 				mask.mouseout(function(){
 					var a = _getAnimProperties($(this)),
 						p = {width:a.w*a.s,height:a.h*a.s,left:a.c_.x,top:a.c_.y},
@@ -167,13 +215,13 @@ function Game(canvas) {
 					_getTile(a.p).stop().css(p).animate(p_, a.d);
 				});
 
+				// define click behaviour
 				mask.click(function(){
-
-					self.numTurns++;
+					_takeTurn($(this).data("pos"));
 					_updateHud();
-
 				});
 
+				// increment the number of tiles on the board
 				self.totalTiles++;
 			}
 		}
@@ -181,9 +229,81 @@ function Game(canvas) {
 		_setAnchor({x:0,y:0});
 	};
 
+	function _takeTurn(pos) {
+		var tile = _getTile("("+pos.x+","+pos.y+")");
+		var isControlled = tile.hasClass("controlled");
+		var colorIndex = tile.data("colorIndex");
+
+		// only allow the turn to be taken if it is an uncontrolled piece
+		if (!isControlled) {
+
+			// check if this tile is isolated, if it is isolated then we know that we
+			// are creating a new anchor
+			if (!_isAdjacentToControlled(pos)) {
+				_setAnchor(pos);
+			} else {
+				// grow the current controlled group
+				_colorSwap(pos,colorIndex);
+			}
+
+			// increment hte number fo turns taken
+			self.numTurns++;
+		}
+	}
+
+	function _isAdjacentToControlled(pos) {
+		for (var i=self.controlled.length-1;i>=0;i--) {
+			if (!self.controlled[i]) continue;
+
+			var x = self.controlled[i].x;
+			var y = self.controlled[i].y;
+			var tile = _getTile("("+x+","+y+")");
+
+			if (pos.x == x-1 && pos.y == y) return true;
+			if (pos.x == x+1 && pos.y == y) return true;
+			if (pos.x == x && pos.y == y-1) return true;
+			if (pos.x == x && pos.y == y+1) return true;
+		}
+		return false;
+	}
+
+	function _colorSwap(pos,colorIndex) {
+
+		// add clicked tile to the controlled group
+		_setControlled(pos);
+
+		// update all existing controlled tiles (and also check if we can add more to the controlled group)
+		for (var i=self.controlled.length-1;i>=0;i--) {
+			if (!self.controlled[i]) continue;
+			
+			var x = self.controlled[i].x;
+			var y = self.controlled[i].y;
+			var tile = _getTile("("+x+","+y+")");
+			var color = "rgb("+self.colors[colorIndex]+")";
+			
+			// animate this tile to the new color
+			var a = {backgroundColor:tile.css("background")}, 
+				a_ = {backgroundColor:color}, 
+				d = self.tile.animDuration;
+			tile.stop().css(a).animate(a_,d);
+
+
+
+			// see if we can omit this tile now, since it is completely blocked off
+			/*
+			if (pos.x == x-1 && pos.y == y && pos.x == x+1 && pos.y == y &&
+				pos.x == x && pos.y == y-1 && pos.x == x && pos.y == y+1) 
+			{
+				self.adjacent[i] = null;
+			}
+			*/
+		}
+	}
+
 	function _updateHud(noAnimation) {
 		var animDuration = noAnimation ? 0 : 200;
 
+		// perform an update anmation of the value has been changed
 		if (self.numTurns != self._numTurns) {
 			fadeInOut($("#turns-taken"),self.numTurns,animDuration);
 		}
@@ -196,11 +316,13 @@ function Game(canvas) {
 		if (self.totalTiles != self._totalTiles) {
 			fadeInOut($("#completion-total"),self.totalTiles,animDuration);
 		}
+		// update the cached values
 		self._numTurns = self.numTurns;
 		self._numTiles = self.numTiles;
 		self._totalTurns = self.totalTurns;
 		self._totalTiles = self.totalTiles;
 
+		// helper function to fade in/out the text upon update
 		function fadeInOut(obj,value,duration) {
 			obj.css({opacity:1.0}).stop().animate({opacity:0.0},duration,function(){
 				$(this).html(value);
@@ -210,12 +332,20 @@ function Game(canvas) {
 	}
 
 	function _setAnchor(pos) {
-		_setControlled(pos);
 		self.anchor = pos;
 
+		// each time a new anchor is set, we will reset the controlled stack
+		self.controlled = [];
+		_setControlled(pos);
+
+		// get references to the appropriate components
 		var tile = _getTile("("+pos.x+","+pos.y+")");
 		var mask = _getTileMask("("+pos.x+","+pos.y+")");
 
+		// update the current color index
+		self.currentColor = tile.data("colorIndex");
+
+		// mark the tile as the new anchor
 		tile.addClass("anchor");
 		mask.addClass("anchor");
 	}
@@ -224,12 +354,20 @@ function Game(canvas) {
 		var tile = _getTile("("+pos.x+","+pos.y+")");
 		var mask = _getTileMask("("+pos.x+","+pos.y+")");
 
+		// mark the tile as controlled
 		tile.addClass("controlled");
 		mask.addClass("controlled");
 
 		mask.css({opacity: OPACITY_CONTROLLED});
+
+		// increment the number of controlled tiles
+		self.numTiles++;
+
+		// push it onto the controlled stack
+		self.controlled.push(pos);
 	}
 
+	// function to report animation properties upon mask hover events
 	function _getAnimProperties(tileobj) {
 		var w = self.tile.width,
 			h = self.tile.height,
@@ -243,6 +381,7 @@ function Game(canvas) {
 		return {w:w,h:h,s:s,p:p,d:d,x:x,y:y,c:c,c_:c_};
 	}
 
+	// helper functions to generate colors on the fly
 	function _generateColor() {
 		return [_getHexValue(),_getHexValue(),_getHexValue()];
 	}
@@ -251,6 +390,7 @@ function Game(canvas) {
 		return Math.min(255,Math.floor(Math.random()*256)+100);
 	}
 
+	// helper functions to get tiles/masks
 	function _getTile(pos) {
 		return $(".tile[pos='"+pos+"']", self.canvas);
 	}
@@ -258,6 +398,7 @@ function Game(canvas) {
 		return $(".tile-mask[pos='"+pos+"']", self.canvas);
 	}
 
+	// returns the literal coordinate of a tile
 	function _getCoord(x,y) {
 		return {
 			x: x*(self.tile.width+self.board.hGap) + self.board.hGap,
