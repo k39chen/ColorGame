8d62247c4482dece24312f5cf76dbc5c7483dd69
Kevin Chen <kchen@tagged.com>
2013-11-16 18:12:53 -0800

    Added a whole slew of functionality and refocused the game objective

--- js/colorGame.js
+++ js/colorGame.js
@@ -27,9 +27,9 @@ var OPACITY_CONTROLLED = 0.0;
 var OPACITY_HOVER = 0.0;
 
 // tile states
-var STATE_ACTIVE = "active";
+var STATE_ACTIVE 	 = "controlled";
 var STATE_CONTROLLED = "controlled";
-var STATE_DORMANT = "dormant";
+var STATE_DORMANT 	 = "dormant";
 
 function Game(canvas) {
 
@@ -41,6 +41,7 @@ function Game(canvas) {
 
 	// game components
 	this.canvas = canvas;
+	this.controlBar = $("#controlBar")
 	this.startMarker = $("#start-marker");
 	this.turnsCounter = $("#turns-container");
 	this.completionCounter = $("#completion-container");
@@ -52,7 +53,7 @@ function Game(canvas) {
 		hGap: 10,
 		vGap: 10,
 		animDuration: 250,
-		seed: 60
+		seed: 100
 	};
 
 	// tile constants
@@ -65,7 +66,9 @@ function Game(canvas) {
 	};
 
 	// set of colors
-	this.numColors = 7;
+	this.numColors = 0;
+	this.minColors = 2;
+	this.maxColors = 7;
 	this.colors = [];
 	this.currentColorIndex = -1;
 
@@ -73,7 +76,7 @@ function Game(canvas) {
 	this.numTiles = 0;
 	this.numTurns = 0;
 	this.totalTiles = 0;
-	this.totalTurns = 200;
+	this.totalTurns = 0;
 
 	// cached game metrics
 	this._numTiles = -1;
@@ -84,7 +87,7 @@ function Game(canvas) {
 	// game variables
 	this.anchor = null;
 	this.controlled = [];
-	this.active = [];
+	this.boundary = [];
 
 	this.start = function() {
 
@@ -99,9 +102,13 @@ function Game(canvas) {
 
 		// first we must create the board!
 		self.clear();
-		self.active = [];
 		self.controlled = [];
+		self.boundary = [];
 		_createBoard();
+		_createControlBar();
+
+		// create modal background
+		_createModal();
 
 		// fade everything into existence
 		self.startMarker.css(o).stop().animate(o_,d);
@@ -143,11 +150,24 @@ function Game(canvas) {
 
 	function _createBoard() {
 
+		// hide the modal message
+		_hideModal();
+
 		// generate the appropriate number of colors
 		self.colors = [];
+		self.numColors = Math.floor(Math.random() * (self.maxColors-self.minColors))+self.minColors;
 		for (var i=0; i<self.numColors; i++) {
 			self.colors.push(_generateColor());
 		}
+
+		// set the maximum number of turns to be some calculated number
+		//   5 (2 colors)  + [1-5]
+		//   10 (3 colors) + [6-10]
+		//   15 (4 colors) + [11-15]
+		//   ...
+		var turnsMin = Math.min(5 * (self.numColors - 1),20);
+		var turnsBuffer = Math.min(2 * (self.numColors - 2),10);
+		self.totalTurns = turnsMin + turnsBuffer;
 
 		// allow the canvas to accept absolute positioned elements within it
 		self.canvas.css({
@@ -225,7 +245,6 @@ function Game(canvas) {
 				// define click behaviour
 				mask.click(function(){
 					_takeTurn($(this).data("pos"));
-					_updateHud();
 				});
 
 				// increment the number of tiles on the board
@@ -236,57 +255,137 @@ function Game(canvas) {
 		_setAnchor({x:0,y:0});
 	};
 
+	function _createControlBar() {
+		self.controlBar.empty();
+
+		self.controlBar.css({
+			marginTop: 16,
+			paddingTop: 8,
+			width: "100%",
+			textAlign: "center"
+		});
+
+		var width = self.tile.width/2;
+		var height = self.tile.height/2;
+
+		for (var i=0; i<self.colors.length; i++) {
+			var container = $("<div>")
+				.addClass("control-tile-container")
+				.data("colorIndex",i)
+				.css({
+					display: "inline-block",
+					position: "relative",
+					width: width,
+					height: height,
+					marginRight: 16
+				})
+				.appendTo(self.controlBar);
+				
+			var tile = $("<div>")
+				.addClass("control-tile")
+				.data("colorIndex",i)
+				.css({
+					position: "absolute",
+					top: 0, left: 0,
+					width: width,
+					height: height,
+					background: "rgb("+self.colors[i]+")",
+					borderRadius: "50%",
+				})
+				.appendTo(container);
+				
+			var mask = $("<div>")
+				.addClass("control-tile-mask")
+				.data("colorIndex",i)
+				.css({
+					position: "absolute",
+					top: 0, left: 0,
+					width: width,
+					height: height,
+					backgroundColor: "black",
+					opacity: OPACITY_NORMAL,
+					borderRadius: "50%",
+					cursor: "pointer"
+				})
+				.appendTo(container);
+
+			mask.mouseover(function(){
+				$(this).css({opacity:OPACITY_NORMAL}).stop().animate({opacity: OPACITY_HOVER}, self.tile.animDuration);
+			});
+			mask.mouseout(function(){
+				$(this).css({opacity:OPACITY_HOVER}).stop().animate({opacity: OPACITY_NORMAL}, self.tile.animDuration);
+			});
+			mask.click(function(){
+				_colorSwap($(this).data("colorIndex"));
+			});
+		}
+	}
+
+	function _createModal() {
+		var modal = $("<div>")
+			.hide()
+			.attr("id","modal")
+			.css({
+				position: "absolute",
+				width: self.canvas.width(),
+				height: self.canvas.height(),
+				background: "rgb(20,20,20)"
+			})
+			.appendTo(self.canvas);
+		var message = $("<div>")
+			.attr("id","modal-message")
+			.css({
+				fontFamily: "HelveticaNeueUltLt",
+				position: "absolute",
+				fontSize: 24,
+				width: "100%",
+				marginTop: 140,
+				textAlign: "center"
+			})
+			.appendTo(modal);
+	}
+
+	function _showModalMessage(text) {
+		$("#modal").show().css({opacity:0}).stop().animate({opacity:0.9},700);
+		$("#modal-message").html(text);
+	}
+	function _hideModal() {
+		$("#modal").css({opacity:0.9}).stop().animate({opacity:0},700,function(){
+			$(this).hide();
+		});
+	}
+
 	function _takeTurn(pos) {
 		var tile = _getTile("("+pos.x+","+pos.y+")");
 		var colorIndex = tile.data("colorIndex");
 
-		// only allow the turn to be taken if it is an uncontrolled piece
-		if (!_isActive(pos)) {
-
-			// update the current color index
-			self.currentColorIndex = tile.data("colorIndex");
-
-			// check if this tile is isolated, if it is isolated then we know that we
-			// are creating a new anchor
-			if (!_isAdjacentToActive(pos)) {
-				_setAnchor(pos);
-			} else {
-				// change the current color to the new one
-				_colorSwap(pos,colorIndex);
-			}
-
-			// increment hte number fo turns taken
-			self.numTurns++;
+		// only process clicks for dormant tiles of different colors than our current color
+		if (_isDormant(pos) && colorIndex != self.currentColorIndex) {
+			// perform actions for swapping colors
+			_colorSwap(colorIndex);
 		}
 	}
 
-	function _isAdjacentToActive(pos) {
-		for (var i=self.active.length-1;i>=0;i--) {
-			if (!self.active[i]) continue;
+	function _colorSwap(colorIndex) {
 
-			var x = self.active[i].x;
-			var y = self.active[i].y;
-			var tile = _getTile("("+x+","+y+")");
+		// the board boundaries
+		var xMin = 0;
+		var xMax = self.board.width - 1;
+		var yMin = 0;
+		var yMax = self.board.height - 1;
 
-			if (pos.x == x-1 && pos.y == y) return true;
-			if (pos.x == x+1 && pos.y == y) return true;
-			if (pos.x == x && pos.y == y-1) return true;
-			if (pos.x == x && pos.y == y+1) return true;
-		}
-		return false;
-	}
+		// update the current color index
+		self.currentColorIndex = colorIndex;
 
-	function _colorSwap(pos,colorIndex) {
+		// see if we can get any adjacent tiles to join our controlled group
+		_checkBoundaries();
 
-		// add clicked tile to the controlled and active group
-		_setActive(pos);
-
-		// update color swap aesthetic for active tiles
-		for (var i=self.active.length-1;i>=0;i--) {
-			if (!self.active[i]) continue;
+		// update color swap aesthetic for controlled tiles
+		for (var i=self.controlled.length-1;i>=0;i--) {
+			if (!self.controlled[i]) continue;
 			
-			var x = self.active[i].x;
-			var y = self.active[i].y;
+			var x = self.controlled[i].x;
+			var y = self.controlled[i].y;
 			var tile = _getTile("("+x+","+y+")");
 			var color = "rgb("+self.colors[colorIndex]+")";
 			
@@ -298,33 +397,65 @@ function Game(canvas) {
 
 			tile.data("colorIndex",colorIndex);
 		}
+
+		// see if we can remove some of those boundary checks
+		self.boundary = [];
+		for (var i=0; i<self.controlled.length; i++) {
+			var pos = self.controlled[i];
+			var tile = _getTile("("+pos.x+","+pos.y+")");
+
+			var adjacent = [
+				{x: pos.x-1, y: pos.y},
+				{x: pos.x+1, y: pos.y},
+				{x: pos.x, y: pos.y-1},
+				{x: pos.x, y: pos.y+1},
+			];
+
+			var numRoutes = 0;
+			for (var j=0; j<adjacent.length; j++) {
+				var p = adjacent[j], x = p.x, y = p.y;
+				var t = _getTile("("+x+","+y+")");
+				var c = t.data("colorIndex");
+
+				// if it satisifes these conditions, then we will continue to 
+				// observe it as a boundary option
+				if (x>=xMin && x<=xMax && y>=yMin && y<=yMax && !_isControlled(p)) {
+					numRoutes++;
+				}
+			}
+			if (numRoutes > 0) {
+				self.boundary.push({x:pos.x,y:pos.y});
+			}
+		}
+
+		// update the player variables
+		self.numTiles = self.controlled.length;
+		self.numTurns++;
+		_updateHud();
 	}
 
 	function _setAnchor(pos) {
 		self.anchor = pos;
 
-		// if we are setting a brand new anchor then we are resetting the
-		// existing set of active tiles
-		_resetActive();
-
 		// get references to the appropriate components
 		var tile = _getTile("("+pos.x+","+pos.y+")");
 		var mask = _getTileMask("("+pos.x+","+pos.y+")");
 
-		// set this as a controlled and active tile
-		_setActive(pos);
+		// update the current color index
+		self.currentColoIndex = tile.data("colorIndex");
+
+		// set this as a controlled tile
+		_setControlled(pos);
+		
+		// update the player variables
+		self.numTiles = self.controlled.length;
+		self.numTurns++;
+		_updateHud();
 	}
 
 	function _setControlled(pos) {
 		var tile = _getTile("("+pos.x+","+pos.y+")");
 		var mask = _getTileMask("("+pos.x+","+pos.y+")");
-
-		// push it onto the controlled stack, if it isn't controlled
-		if (!_isControlled(pos)) {
-			self.controlled.push(pos);
-		}
-		// update the number of controlled tiles
-		self.numTiles = self.controlled.length;
 
 		// mark the tile as controlled
 		tile.removeClass(STATE_DORMANT).addClass(STATE_CONTROLLED);
@@ -332,81 +463,55 @@ function Game(canvas) {
 
 		// update aesthetic for controlled groups
 		mask.css({opacity: OPACITY_CONTROLLED});
-	}
-
-	function _setActive(pos) {
-		var tile = _getTile("("+pos.x+","+pos.y+")");
-		var mask = _getTileMask("("+pos.x+","+pos.y+")");
-
-		// mark the tile as active
-		tile.removeClass(STATE_DORMANT).addClass(STATE_ACTIVE);
-		mask.removeClass(STATE_DORMANT).addClass(STATE_ACTIVE);
-
-		// update aesthetic for active groups
 		tile.css({boxShadow: "0px 0px 10px rgba(255,255,255,1.0)"});
 
-		// if this tile is active, then it must also be controlled
-		_setControlled(pos);
+		// push it onto the controlled stack
+		self.controlled.push(pos);
+		self.boundary.push(pos);
+		self.currentColorIndex = tile.data("colorIndex");
 
-		// push it onto the active stack
-		self.active.push(pos);
-
-		// see if we can get any more active with adjacent tiles!
-		_checkNeighbours();
+		// see if we can get any more controlled with adjacent tiles!
+		_checkAdjacent(pos);
 	}
 
-	function _checkNeighbours() {
+	function _checkBoundaries() {
+		for (var i=0; i<self.boundary.length; i++) {
+			_checkAdjacent(self.boundary[i]);
+		}
+	}
+
+	function _checkAdjacent(pos) {
 		var xMin = 0;
 		var xMax = self.board.width - 1;
 		var yMin = 0;
 		var yMax = self.board.height - 1;
 
-		var queue = self.active.slice(0);
+		var tile = _getTile("("+x+","+y+")");
 
-		while (queue.length > 0) {
-			var pos = queue.shift();
+		// define the coordinates of the adjacent tiles
+		var adjacentTiles = [
+			{x: pos.x-1, y: pos.y},
+			{x: pos.x+1, y: pos.y},
+			{x: pos.x, y: pos.y-1},
+			{x: pos.x, y: pos.y+1}
+		];
 
-			var adjacentTiles = [
-				{x: pos.x-1, y: pos.y},
-				{x: pos.x+1, y: pos.y},
-				{x: pos.x, y: pos.y-1},
-				{x: pos.x, y: pos.y+1}
-			];
+		// check if any of the adjacent tiles can be added to the controlled group
+		for (var i=0; i<adjacentTiles.length; i++) {
+			var p = adjacentTiles[i], x = p.x, y = p.y;
+			var adjacentTile = _getTile("("+x+","+y+")");
+			var colorIndex = adjacentTile.data("colorIndex");
 
-			for (var i=0; i<adjacentTiles.length; i++) {
-				var p = adjacentTiles[i], x = p.x, y = p.y;
-				var tile = _getTile("("+x+","+y+")");
-				var colorIndex = tile.data("colorIndex");
-			
-				// if it doesn't satisfy our constraints then don't bother looking
-				// at this neighbouring tile
-				if (x < xMin || x > xMax || y < yMin || y > yMax) continue;
-
-				// check if this tile is already in our active list
-				var isActive = _isActive(p);
-
-				// only add inactive tiles that match our color of interest
-				if (!isActive && colorIndex == self.currentColorIndex) {
-					_setActive(p);
-				}
+			// is not blocked only if this adjacent tile respects the board boundaries
+			// and is an uncontrolled tile that matches our color of interest
+			if (x >= xMin && x <= xMax && y >= yMin && y <= yMax &&
+				!_isControlled(p) && colorIndex == self.currentColorIndex)
+			{
+				// mark this tile as a controlled tile, and subsequently check its adjacent
+				// tiles as well!
+				_setControlled(p);
 			}
 		}
-	}
-
-	function _resetActive() {
-		// go through the current set of active tiles and strip them of active status
-		for (var i=0; i<self.active.length; i++) {
-			if (!self.active[i]) continue;
-
-			var x = self.active[i].x;
-			var y = self.active[i].y;
-			var tile = _getTile("("+x+","+y+")");
-
-			// update aesthetic for active groups
-			tile.css({boxShadow: "0px 0px 10px rgba(255,255,255,0.0)"})
-				.removeClass(STATE_ACTIVE);
-		}
-		self.active = [];
 	}
 
 	function _updateHud(noAnimation) {
@@ -437,6 +542,69 @@ function Game(canvas) {
 				$(this).html(value);
 				$(this).css({opacity:0.0}).stop().animate({opacity:1.0},duration);
 			});
+		}
+
+
+		// determine what game state we are in now
+		var modalMessage = "";
+		if (self.numTiles == self.totalTiles) {
+			var easeOfCompletion = self.numTurns / self.totalTurns
+			if (easeOfCompletion == 1.00) {
+				modalMessage = 
+					"<b>BY THE SKIN OF YOUR TEETH!</b><br>"+
+					"You have successfully controlled "+
+					"<b>"+self.totalTiles+"</b>"+" tiles with no turns remaining!";
+			} else if (easeOfCompletion >= 0.75) {
+				modalMessage = 
+					"<b>Congratulations!</b><br>"+
+					"You have successfully controlled "+
+					"<b>"+self.totalTiles+"</b>"+" tiles in "+
+					"<b>"+self.numTurns+"</b> turns!";
+			} else if (easeOfCompletion >= 0.5) {
+				modalMessage = 
+					"<b>WOOOW!</b>"+
+					"<br>You have successfully controlled "+
+					"<b>"+self.totalTiles+"</b>"+" tiles in just"+
+					"<b>"+self.numTurns+"</b> turns!";
+			} else {
+				modalMessage = 
+					"<b>AMAZING!</b><br>"+
+					"You have successfully controlled "+
+					"<b>"+self.totalTiles+"</b>"+" tiles in only "+
+					"<b>"+self.numTurns+"</b> turns!";
+			}
+			_showModalMessage(modalMessage);
+			return;
+		}
+		if (self.numTurns >= self.totalTurns) {
+			var completion = self.numTiles / self.totalTiles;
+			if (completion >= 0.90) {
+				modalMessage = 
+				"<b>SO CLOSE!</b><br/>"+
+				"You managed to control "+
+				"<b>"+Math.floor(completion*100)+"%</b> "+
+				"of the board."
+			} else if (completion >= 0.60) {
+				modalMessage = 
+				"<b>GAME OVER!</b><br/>"+
+				"You only managed to control "+
+				"<b>"+Math.floor(completion*100)+"%</b> "+
+				"of the board."
+			} else if (completion >= 0.40) {
+				modalMessage = 
+				"<b>WHAT ARE YOU DOING?</b><br/>"+
+				"You only managed to control "+
+				"<b>"+Math.floor(completion*100)+"%</b> "+
+				"of the board."
+			} else {
+				modalMessage = 
+				"<b>YOU'RE SO BAD YOU SHOULD PLAY AGAIN</b><br/>"+
+				"You only managed to control "+
+				"<b>"+Math.floor(completion*100)+"%</b> "+
+				"of the board."
+			}
+			_showModalMessage(modalMessage);
+			return;
 		}
 	}
 
@@ -480,7 +648,6 @@ function Game(canvas) {
 	}
 
 	// state checkers
-	function _isActive(pos) { return _getTile("("+pos.x+","+pos.y+")").hasClass(STATE_ACTIVE); }
 	function _isControlled(pos) { return _getTile("("+pos.x+","+pos.y+")").hasClass(STATE_CONTROLLED); }
 	function _isDormant(pos) { return _getTile("("+pos.x+","+pos.y+")").hasClass(STATE_DORMANT); }
 
